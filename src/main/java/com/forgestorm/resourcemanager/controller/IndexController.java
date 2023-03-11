9de476d2f37c558a475f882e2dce5db15583ff9d
unenergizer <unenergizer@gmail.com>
2023-03-11 09:37:53 -0600

    Moved controller's to controller package

--- src/main/java/com/forgestorm/resourcemanager/controller/IndexController.java
+++ src/main/java/com/forgestorm/resourcemanager/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.forgestorm.resourcemanager;
+package com.forgestorm.resourcemanager.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
